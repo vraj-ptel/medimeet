53c9e647cdd674dc44b2b44a04a5a984bedfe8b5
--get <106588213+vraj-ptel@users.noreply.github.com>
2025-06-16 13:55:18 +0530

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "./generated/prisma";
 
-const prisma =globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+// const prisma =globalThis.prisma || new PrismaClient();
+const prisma=new PrismaClient()
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 export default prisma;
 
